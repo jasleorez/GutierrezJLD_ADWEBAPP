d4a2e72d2d11de156985789b6c63d1d7b16234e8
jasleorez <123354200+jasleorez@users.noreply.github.com>
2025-01-20 13:02:09 +0800

    Add files via upload

---
+++ Midterm LabAct4_Sharing_Local_Data_Across_Components/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from './employee.service';
+import { ProductsService } from './products.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'services-demo'
+
+  public employees: {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+  }[] = [];
+
+  public products: {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+  }[] = [];
+
+  constructor(
+    private _employeeService: EmployeeService,
+    private _productsService: ProductsService
+  ) { } 
+  
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+    this.products = this._productsService.getProducts();
+  }
+}
+  // [
+  // {
+  //   id: 101,
+  //   firstname: 'Joseph',
+  //   lastname:'Dizon',
+  //   email:'jdizon@hau.edu.ph',
+  // },
+  // {
+  //   id: 102,
+  //   firstname: 'James',
+  //   lastname: 'Atienza',
+  //   email: 'jatienza@hau.edu.ph',
+  // },
+  // {
+  //   id: 103,
+  //   firstname: 'John',
+  //   lastname: 'Cena',
+  //   email:'jcena@hau.edu.ph',
+  // },
+  // {
+  //   id: 104,
+  //   firstname: 'Jasper',
+  //   lastname: 'Gutierrez',
+  //   email: 'jdgutierrez@student.hau.edu.ph',
+  // }
+
+  // ];
+
